b9f49b2dc74a5191c151f15e8dd3225230b22476
Ben Bagwell <dualbravo@gmail.com>
2023-11-04 07:31:44 -0400

    Added file structure, boilerplate classes. Updated build.gradle and template.yaml

---
+++ PopStockLambda/src/main/java/com/nashss/se/popstock/dependency/ServiceComponent.java
@@ -0,0 +1,15 @@
+package com.nashss.se.popstock.dependency;
+
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+/**
+ * Dagger component for providing dependency injection in the Music Playlist Service.
+ */
+@Singleton
+@Component(modules = {DaoModule.class, MetricsModule.class})
+public interface ServiceComponent {
+
+
+}
