b9d20f9e7c0ee4bd0df8bc33ce16b81d4ff58240
Ben Bagwell <dualbravo@gmail.com>
2023-12-09 21:05:30 -0500

    wip

--- PopStockLambda/src/main/java/com/nashss/se/popstock/dynamodb/TransactionDao.java
+++ PopStockLambda/src/main/java/com/nashss/se/popstock/dynamodb/TransactionDao.java
@@ -1,10 +1,14 @@
 package com.nashss.se.popstock.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.nashss.se.popstock.dynamodb.models.Transaction;
+import com.nashss.se.popstock.dynamodb.models.Warehouse;
 import com.nashss.se.popstock.metrics.MetricsPublisher;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class TransactionDao {
@@ -18,5 +22,18 @@ public class TransactionDao {
         this.metricsPublisher = metricsPublisher;
     }
 
+    public void deleteTransaction(Transaction transaction) {
+        dynamoDBMapper.delete(transaction);
+    }
+
+    public List<Transaction> getTransactions(String warehouseId) {
+        Transaction transactionPartition = new Transaction();
+        transactionPartition.setWarehouseId(warehouseId);
+
+        DynamoDBQueryExpression<Transaction> dynamoDBQueryExpression = new DynamoDBQueryExpression<Transaction>()
+                .withHashKeyValues(transactionPartition);
+
+        return this.dynamoDBMapper.query(Transaction.class, dynamoDBQueryExpression);
+    }
 
 }
